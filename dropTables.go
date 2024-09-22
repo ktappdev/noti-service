1542ada5dd415a06bdb68406aede9bbe10041f44
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-09-22 09:03:41 -0400

    upd

--- dropTables.go
+++ dropTables.go
@@ -3,6 +3,8 @@ package main
 func dropTables() error {
 	_, err := db.Exec(`
         DROP TABLE IF EXISTS notifications;
+        DROP TABLE IF EXISTS product_owners_notifications;
+        DROP TABLE IF EXISTS user_notifications;
         DROP TABLE IF EXISTS owners;
         DROP TABLE IF EXISTS users;
     `)
